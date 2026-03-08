4ddbb446042def26244de799b1193fc8bdad5981
vanetprojectbe <vanetprojectbe@gmail.com>
2026-03-08 12:18:29 +0530

    Create api.js

---
+++ src/config/api.js
@@ -0,0 +1,7 @@
+const API_BASE = "https://cms-backend-lmof.onrender.com";
+
+export const API = {
+  accidents: `${API_BASE}/api/accidents`,
+  rsu: `${API_BASE}/api/rsu`,
+  login: `${API_BASE}/api/auth/login`
+};
